9fbe6e91a71ff6b0907d8ce2c6a4b70d0b87865b
Chris Wash <chris.wash@gmail.com>
2014-07-03 17:17:37 -0400

    Complete groovy implementation

--- ex2-groovy/src/main/groovy/org/cwash/ex2groovy/Exercise2Groovy.groovy
+++ ex2-groovy/src/main/groovy/org/cwash/ex2groovy/Exercise2Groovy.groovy
@@ -3,9 +3,58 @@ package org.cwash.ex2groovy
 /**
  * Exercise2Groovy Groovy class.
  */
-class Exercise2Groovy
-{
-    def show() {
-        println 'Hello World'
+class Exercise2Groovy {
+
+    def List<String> mostPopularThreePageSequence(List<LogLine> logFileInput) {
+
+        /* find unique users for partitions */
+        def uniqueUsers = [] as TreeSet
+        logFileInput.each { uniqueUsers.add(it.customerId) }
+
+        /* partition the dataset by unique user */
+        def usersToLogLinesMap = [:] as TreeMap
+        uniqueUsers.each { user ->
+            def logLinesForUser = logFileInput.findAll { it.customerId == user }
+            usersToLogLinesMap[user] = logLinesForUser
+        }
+
+        def sequenceSize = 3;
+
+
+        def linePermutationsToCount = [:] as Map<List<String>, Integer>
+
+        /* generate permutations for all of the page sequences */
+        uniqueUsers.each { user ->
+            def logLinesForUser = usersToLogLinesMap.get(user)
+
+            for (int i = 0; i < logLinesForUser.size(); i++) {
+                def linePermutation = []
+                for (int j = 0; j < sequenceSize; j++) {
+                    if (i+j < logLinesForUser.size()) {
+                        linePermutation.add(logLinesForUser.get(i+j).pageType)
+                    } else {
+                        break;
+                    }
+                }
+
+                if (linePermutation.size() == sequenceSize)
+                {
+                    int lastCount = linePermutationsToCount[linePermutation];
+                    if (lastCount == null) {
+                        linePermutationsToCount[linePermutation] = 1;
+                    } else {
+                        int count = lastCount + 1
+                        linePermutationsToCount[linePermutation] = count;
+                    }
+                }
+
+            }
+
+
+        }
+
+        /* rank sequences by count and return most popular sequence */
+        def mostPopularSequence = linePermutationsToCount.max { it.value }.key
+        return mostPopularSequence;
     }
 }
--- ex2-groovy/src/test/groovy/org/cwash/ex2groovy/Exercise2GroovyTest.groovy
+++ ex2-groovy/src/test/groovy/org/cwash/ex2groovy/Exercise2GroovyTest.groovy
@@ -1,15 +1,48 @@
 package org.cwash.ex2groovy
-
-import org.cwash.ex2groovy.Exercise2Groovy
+import org.junit.Before
 import org.junit.Test
 
+import static org.hamcrest.CoreMatchers.equalTo
+import static org.junit.Assert.assertThat
 /**
  * Tests for the {@link org.cwash.ex2groovy.Exercise2Groovy} class.
  */
 class Exercise2GroovyTest
 {
+
+
+    public static final List<LogLine> EXAMPLE_INPUT = Arrays.asList(
+            new LogLine("cust1", "Gateway", 1404361817342l),
+            new LogLine("cust8", "Gateway", 1404361817352l),
+            new LogLine("cust8", "Gateway", 1404361817362l),
+            new LogLine("cust5", "Detail", 1404361817372l),
+            new LogLine("cust1", "Search", 1404361817382l),
+            new LogLine("cust8", "Search", 1404361817392l),
+            new LogLine("cust1", "Detail", 1404361817402l),
+            new LogLine("cust5", "Search", 1404361817412l),
+            new LogLine("cust5", "YourAccount", 1404361817422l),
+            new LogLine("cust5", "Detail", 1404361817432l),
+            new LogLine("cust8", "Detail", 1404361817442l),
+            new LogLine("cust8", "Gateway", 1404361817452l),
+            new LogLine("cust2", "Help", 1404361817462l)
+    );
+
+
+    private Exercise2Groovy exercise2Groovy;
+
+    private List<LogLine> testInput;
+
+
+    @Before
+    public void setUp() throws Exception {
+        exercise2Groovy = new Exercise2Groovy();
+
+        testInput = EXAMPLE_INPUT;
+    }
+
     @Test
-    void testShow() {
-        new Exercise2Groovy().show()
+    void testMostPopularThreePageSequence() throws Exception {
+        String actual = exercise2Groovy.mostPopularThreePageSequence(testInput).toString();
+        assertThat(actual, equalTo("[Gateway, Search, Detail]"));
     }
 }
